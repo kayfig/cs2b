ad6b90533791484d9d687c4e71e6b9913f9df844
kayfig <kamryn-rose.figuerres@smash.lpfi.org>
2017-10-08 13:16:44 -0700

    upload

---
+++ JokeTeller.py
@@ -0,0 +1,40 @@
+# Joke Teller
+# A game by Kamryn Figuerres
+# October 10, 2017
+
+# This game simply tells some jokes to the player.
+# Really it's just an excuse for me to practice a few things
+# to prepare for our next big game.
+
+# All jokes were stolen from the internet.
+
+# Double Quotes 
+print("Why do we tell actors to break a leg?")
+input()
+print("Every play has a cast!")
+print("***HAAAAAAAA***")
+print()
+
+# Single quotes and escape characters
+print('Did you hear about the actor who fell through the floorboards?')
+input()
+print('He was just going through a stage!')
+print('***HARDY HAR HAR!!!***')
+print()
+
+print('Hear about the new restaurant called Karma?')
+input()
+print('There\'s no menu: You get what you deserve')
+print('***LOLOLOLOL***')
+print()
+
+print('What did the tin man say when he got run over by a steamroller?')
+input()
+print('\"Curses! Foil again!\"')
+print()
+      
+print('Why did the physics teacher break up with the biology teacher?', end="")
+input()
+print('There was no chemistry!')
+print('***LMAOOOO***')
+print()
